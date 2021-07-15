c8d7aee1c6af2e350927dce0d17b76cdeccce580
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-07-15 10:13:46 -0500

    Test

---
+++ src/main/java/edu/escuelaing/arsw/testingWeb/TestingWebApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.testingWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWebApplication.class, args);
+	}
+
+}
